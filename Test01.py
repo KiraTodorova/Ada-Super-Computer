440a6cf2eea4f5eee8b6c55b27b202eee41548f4
Mauro Strandberg <mauro.strandberg@gmail.com>
2020-08-13 21:22:36 +0200

    Commited Python Code

---
+++ Test01.py
@@ -0,0 +1,55 @@
+def HelloWorld ():
+	print ("")
+	print("Hello world!!! I haven't missed You for a second!! >:3")
+	print ("")
+
+
+
+def MyAssHurts ():
+	print ("My ass hurts.")
+	print ("")
+
+def AdamIsAGenius ():
+	print ("HURRAAAAAAAAAY!!!!! Adam, You are a genius!!!!")
+	print ("")
+
+def InMyAssHole ():
+	print ("In my asshole.")
+	print ("")
+
+def ThereShouldBeOnePersonHandelingThisShit():
+	print("There should be only one slave doing this kind of work!!! Five slaves are too expensive!!!!")
+	print ("")
+
+def AngryBastard():
+	print ("I'm hungry and I haven't eaten for the whole week!! I don't even know how I can be alive!! Oh, right!! This is a game!!! Right??! Now give me the motherblessing burrito!!! I'm starving, You piece of badabipidaboop!!!!")
+	print ("")
+
+HelloWorld()
+
+MyAssHurts()
+
+AdamIsAGenius()
+
+InMyAssHole ()
+
+ThereShouldBeOnePersonHandelingThisShit()
+
+AngryBastard()
+
+
+
+
+lst = ["Burrito", "Panini", "Nachos", "Olives", "Pizza", "Pasta", "Bullshit"]
+
+print ("Today's menu is the one below this useless advert:")
+print("")
+
+print (lst [0], lst [2], lst [3], lst [6])
+print("")
+print("Have a nice motherblessing day!!! And stop annoying me for FOX SAKE!!!!!")
+print("")
+print("--     END OF MESSAGE     -- ")
+print("")
+print(" -  Remember to keep all your mails, our super annoying supremacist highness in my asshole!!!  -")
+print("")
